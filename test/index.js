050a7b28065e31c73be9a4daafb15e32f39f2354
Kenan Yildirim <kenan@kenany.me>
2019-09-02 14:27:59 -0400

    deps: eslint@6.3.0, eslint-config@7.0.0
    Closes https://github.com/KenanY/sowpods-six/pull/21
    Closes https://github.com/KenanY/sowpods-six/pull/23

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-var sowpodsSix = require('../');
 var test = require('tape');
 var isArray = require('lodash.isarray');
+
+var sowpodsSix = require('../');
 
 test('exports an array', function(t) {
   t.plan(1);
