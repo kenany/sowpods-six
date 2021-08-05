0b2e1b3c266f8c93a05b81d9ae6ac2500dffe0eb
Kenan Yildirim <kenan@kenany.me>
2021-08-05 16:39:22 -0400

    feat(ci): add Node.js v16, remove Node.js v10
    BREAKING CHANGE: Node.js v10 is no longer supported.

--- test/index.js
+++ test/index.js
@@ -1,9 +1,8 @@
 const test = require('tape');
-const isArray = require('lodash.isarray');
 
 const sowpodsSix = require('../');
 
 test('exports an array', function(t) {
   t.plan(1);
-  t.ok(isArray(sowpodsSix));
+  t.ok(Array.isArray(sowpodsSix));
 });
